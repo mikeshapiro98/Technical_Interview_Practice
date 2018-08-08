913484b88e0fa74f5f44f842f062108337f2a0ea
Mike Shapiro <michaelshapiro98@gmail.com>
2018-08-08 14:13:12 -0700

    Adding Challenge 2

---
+++ Challenge 2/Adders.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_2
+{
+    class Adders
+    {
+        public decimal bigNum (int[] i)
+        {
+            int intAdd = i.Sum();
+            decimal longAdd = Convert.ToDecimal(intAdd);
+            return longAdd;
+        }
+    }
+}
---
+++ Challenge 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adders adders = new Adders();            
+            int[] testArray = new int[] {1, 1773, 2847, 8371, 77875, 973636, 62542738  };
+            decimal tots = adders.bigNum(testArray);
+            Console.WriteLine(tots);
+            Console.ReadLine();
+        }
+    }
+}
+
+//Given an array of integers, write a method to sum the elements in the array, knowing that some of the elements may be very large integers. 
