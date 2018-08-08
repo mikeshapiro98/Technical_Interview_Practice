ac66211f8521d35c76b9d1f27a7a093dd95f27b2
Mike Shapiro <michaelshapiro98@gmail.com>
2018-08-08 15:44:08 -0700

    Add Challenge 4

---
+++ Challenge 4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString = "This should apear with no duplicate characters.";
+            char[] testArray = testString.ToCharArray();
+            foreach(char i in testArray)
+            {
+                Console.WriteLine(i);
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+//Given a string, remove any repeated letters. 
---
+++ Challenge 4/dupRemove.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_4
+{
+    class dupRemove
+    {
+        //public string duplicater(string[] s)
+        //{
+        //    for(int i = 0; i < s.Length; i++)
+        //    {
+
+        //    }
+              
+        //}
+
+    }
+}
