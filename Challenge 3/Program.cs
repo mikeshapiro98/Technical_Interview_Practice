21182ebda3b0df8317d846a04b128e0f71a03381
Mike Shapiro <michaelshapiro98@gmail.com>
2018-08-08 15:08:23 -0700

    Add Challenge 3

---
+++ Challenge 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            reverser reverser = new reverser();
+            string testString = "Hello World Of Wonder";
+            string tots = reverser.rever(testString);
+            Console.WriteLine(testString);
+            Console.WriteLine(tots);
+            Console.ReadLine();
+        }
+    }
+}
+
+//Given a string, reverse it.
---
+++ Challenge 3/reverser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_3
+{
+    class reverser
+    {
+        public string rever (string s)
+        {
+            char[] reverse = s.ToCharArray();
+            Array.Reverse(reverse);
+            return new string(reverse); 
+        }
+    }
+}
