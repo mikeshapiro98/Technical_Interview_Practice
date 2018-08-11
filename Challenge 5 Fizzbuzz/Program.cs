1053f51c54eae69e4ef5c29bd4e2ecc73ba4b50f
Mike Shapiro <michaelshapiro98@gmail.com>
2018-08-11 14:25:13 -0700

    Add Challenge 5 Fizzbuzz

---
+++ Challenge 5 Fizzbuzz/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_5_Fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if(i%3==0 && i%5==0)
+                {
+                    Console.WriteLine("Fizzbuzz");
+
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if(i%5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
+//Write a short program that prints each number from 1 to 100 on a new line.
+
+//For each multiple of 3, print "Fizz" instead of the number.
+
+//For each multiple of 5, print "Buzz" instead of the number. 
+
+//For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
