438cced79ee9f2740d4ca722b0a124260c1c56cf
Mike Shapiro <michaelshapiro98@gmail.com>
2019-01-19 17:15:53 -0800

    X's and O's

---
+++ X's and O's/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X_s_and_O_s
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+//CHALLENGE:
+
+//Check to see if a string has the same amount of 'x's and 'o's.The method must return a boolean and be case insensitive.The string can contain any char.
+
+//Examples input/output:
+//XO("ooxx") => true
+//XO("xooxx") => false
+//XO("ooxXm") => true
+//XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+//XO("zzoo") => false
