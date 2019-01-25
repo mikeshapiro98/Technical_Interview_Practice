6633f87544f4f27e372a77706637f9ce42d17ab9
Mike Shapiro <michaelshapiro98@gmail.com>
2019-01-25 12:52:25 -0800

    Add Prompt

--- doubler/Program.cs
+++ doubler/Program.cs
@@ -18,3 +18,14 @@ namespace doubler
         }
     }
 }
+
+//CHALLENGE:
+
+//Given a string, you have to return a string in which each character(case-sensitive) is repeated once.
+
+//DoubleChar("String") == "SSttrriinngg"
+
+//DoubleChar("Hello World") == "HHeelllloo  WWoorrlldd"
+
+//DoubleChar("1234!_ ") == "11223344!!__  "
+
