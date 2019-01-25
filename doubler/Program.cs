83a429135eee2ea7afb7c3d975b4a1b6388a594d
Mike Shapiro <michaelshapiro98@gmail.com>
2019-01-25 12:35:23 -0800

    New interview pracitce question

---
+++ doubler/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doubler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            duplicate Duplicate = new duplicate();
+            string tester = Duplicate.makeDouble("abcde");
+            Console.WriteLine(tester);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ doubler/duplicate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doubler
+{
+    class duplicate
+    {
+        
+        public string makeDouble(string s)
+        {
+            foreach(char )
+
+            return 
+        }
+
+    }
+}
