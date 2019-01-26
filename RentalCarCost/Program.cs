ace1f94201c800beee5a5deaf439a707b092a392
Mike Shapiro <michaelshapiro98@gmail.com>
2019-01-26 02:17:19 -0800

    Add Car Rental

--- RentalCarCost/Program.cs
+++ RentalCarCost/Program.cs
@@ -10,6 +10,14 @@ namespace RentalCarCost
     {
         static void Main(string[] args)
         {
+            RentalCar rental = new RentalCar();
+            Console.WriteLine("HOW MANY DAYS WILL YOU BE RENTING THE CAR?");
+            int days = Convert.ToInt32(Console.ReadLine());
+            int display = RentalCar.RentalCarCost(days);
+            Console.WriteLine(display);
+
+            Console.ReadLine();
+
         }
     }
 
@@ -18,24 +26,32 @@ namespace RentalCarCost
 
         public static int RentalCarCost(int d)
         {
-            decimal cost = 0.00m;
-            for (int i = 0; i < d; i++)
+            int cost = 0;
+           
+            if (d >= 7)
             {
-                if (d >= 7)
-                {
-                    cost = d * 40 - 50.00m;
-                }
-
+                cost = d * 40 - 50;
             }
+            else if (d >= 3)
+            {
+                cost = d * 40 - 20;
+            }
+            else
+            {
+                cost = d * 40;
+            }
+            return cost;
         }
     }
 
 }
+
+// return d >= 7 ? d * 40 - 50 : d >= 3 ? d * 40 - 20 : d* 40;
+//return d*40-(d>6?50:d>2?20:0);
 //CHALLENGE
 //After a hard quarter in the office you decide to get some rest on a vacation.So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
 
 //You will need a rental car in order for you to get around in your vacation.The manager of the car rental makes you some good offers.
-
 
 //Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total.Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
 
