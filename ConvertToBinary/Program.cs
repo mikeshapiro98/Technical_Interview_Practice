4c8e82fe4e91df09686c6c40525dea8ef65722f1
Mike Shapiro <michaelshapiro98@gmail.com>
2019-01-27 13:19:48 -0800

    Add new challenge

---
+++ ConvertToBinary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(binary.C2B(1, 2));
+            Console.ReadLine(); 
+        }
+    }
+
+    static class binary
+    {
+        public static string C2B (int a, int b)
+        {
+          //  string result = ConvertToBinary(a + b);
+            return Convert.ToString(a+b, 2); 
+        }
+
+
+    }
+
+}
+
+//CHALLENGE: 
+//Implement a function that adds two numbers together and returns their sum in binary.The conversion can be done before, or after the addition.
+
+//The binary number returned should be a string.
