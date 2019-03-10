7e67cf2c4d00b8889e90ef533f40c276b75eed5b
Mike Shapiro <michaelshapiro98@gmail.com>
2019-03-10 11:57:06 -0700

    Add new Whiteboard

---
+++ HighestLowest/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace HighestLowest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tester = { 700, 4, 55, 325, 2, 1 };
+            int[] view = fun.highestLowest(tester);
+            foreach(int num in view)
+            {
+                WriteLine(num);
+            }
+           
+            ReadLine();
+        }
+    }
+    class fun
+    {
+        public static int[] highestLowest(int[] lst)
+        {
+            int[] res = new int[2];
+            res[0] = lst.Min();
+            res[1] = lst.Max();
+            return res; 
+        }
+    }
+}
+
+//using System;
+
+//public class MinMax
+//{
+//    public static int[] minMax(int[] lst)
+//    {
+//        int min = lst[0];
+//        int max = lst[0];
+//        foreach (int num in lst)
+//        {
+//            if (num > max)
+//            {
+//                max = num;
+//            }
+//            if (num < min)
+//            {
+//                min = num;
+//            }
+//        }
+//        return new int[] { min, max };
+//    }
+//}
