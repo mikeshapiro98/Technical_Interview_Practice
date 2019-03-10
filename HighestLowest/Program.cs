aa29d8b22883886fd902cfa9a5cffa0e4fab091a
Mike Shapiro <michaelshapiro98@gmail.com>
2019-03-10 12:07:50 -0700

    Add Prompt

--- HighestLowest/Program.cs
+++ HighestLowest/Program.cs
@@ -56,3 +56,8 @@ namespace HighestLowest
 //        return new int[] { min, max };
 //    }
 //}
+
+    //PROMPT
+//Ben has a very simple idea to make some profit: he buys something and sells it again.Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
+
+//Write a function that returns both the minimum and maximum number of the given list/array. 
