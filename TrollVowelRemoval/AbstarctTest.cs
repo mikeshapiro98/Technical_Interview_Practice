d3d85df21e12d6a80018ce146727aa93f4a4e9e7
Mike Shapiro <michaelshapiro98@gmail.com>
2019-03-23 10:30:30 -0700

    Class Testing

---
+++ TrollVowelRemoval/AbstarctTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrollVowelRemoval
+{
+    abstract class AbstarctTest
+    {
+
+        public int Adder(int a, int b)
+        {
+            return (a + b);
+        }
+    }
+}
