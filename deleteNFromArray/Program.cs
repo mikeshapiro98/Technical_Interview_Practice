8e2cf0e90a3876f536c6e9726ae2f45b0a0f2bad
Mike Shapiro <michaelshapiro98@gmail.com>
2019-04-05 13:14:04 -0700

    Update. Will revisit later

--- deleteNFromArray/Program.cs
+++ deleteNFromArray/Program.cs
@@ -10,12 +10,30 @@ namespace deleteNFromArray
     {
         static void Main(string[] args)
         {
+            int[] tester = new int[] { 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 6, 6, 6, 7, 7, 7 };
+            int [] tester2 = RemoveN.DeleteNth(tester, 2);
+            foreach(int n in tester2)
+            {
+                Console.WriteLine(n);
+            }
+            Console.ReadLine();
         }
     }
     class RemoveN
     {
-        public int[] 
+        public static int[] DeleteNth(int[] arr, int x)
+        {
+            int[] results; //=  new int[] { arr.Where(g => g.Count() < x)};
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i])
+            }
+            return results;
+        }
     }
 
 
 }
+//Task
+
+//Given a list lst and a number N, create a new list that contains each number of lst at most N times without reordering.For example if N = 2, and the input is [1,2,3,1,2,1,2,3], you take[1, 2, 3, 1, 2], drop the next[1, 2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to[1, 2, 3, 1, 2, 3].
