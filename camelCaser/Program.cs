61624fe01160a0bd680a4b8956eee16c9bf751e2
Mike Shapiro <michaelshapiro98@gmail.com>
2019-04-09 13:46:14 -0700

    Remove bad comments

--- camelCaser/Program.cs
+++ camelCaser/Program.cs
@@ -21,18 +21,13 @@ namespace camelCaser
             char[] charArray = s.ToCharArray();
             string d2 = "";
             bool isIt = true;
-            //foreach(char c in charArray)
-            //{
-            //    Console.WriteLine(c); 
-            //}
-            //s[0] = char.ToUpper(s[0]);
+
             for (int i = 0; i < charArray.Length; i++)
             {
                  if (charArray[i] == ' ')
                 {
                     isIt = true;
-                    // d2 += char.ToUpper(charArray[i + 1]);
-                    // charArray[i] = 
+
                 }
                 else  if (isIt)
                 {
@@ -46,11 +41,22 @@ namespace camelCaser
                     d2 += charArray[i];
                 }
             }
-          //  string d2 = new string(charArray);
+       
             return d2;
-           // s = string.Join(d2)
+         
         }
     }
 }
 
 
+// d2 += char.ToUpper(charArray[i + 1]);
+// charArray[i] = 
+
+//foreach(char c in charArray)
+//{
+//    Console.WriteLine(c); 
+//}
+//s[0] = char.ToUpper(s[0]);
+
+//  string d2 = new string(charArray);
+// s = string.Join(d2)
